e634025c928fa6445d4c8bff54dc2dd0c124f0fa
arugerdev <alvaroch250@gmail.com>
2024-05-30 23:50:55 +0200

    Update vercel serverless function

--- api/draw.js
+++ api/draw.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.VITE_SUPABASE_URL
+const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default async function handler (req, res) {
