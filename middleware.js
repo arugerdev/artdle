4c6cee3b7ecf83f8b761cd760c37107cf1dbaea4
arugerdev <alvaroch250@gmail.com>
2024-05-31 15:46:12 +0200

    Remove try and catch

--- middleware.js
+++ middleware.js
@@ -23,54 +23,49 @@ export default async function middleware (req, res) {
 
   const drawId = req.url.split('/')[1]
 
-  try {
-    const { data, error } = await supabase
-      .from('draws')
-      .select('*')
-      .eq('id', drawId)
+  const { data, error } = await supabase
+    .from('draws')
+    .select('*')
+    .eq('id', drawId)
 
-    if (error) {
-      console.error('Error fetching data from Supabase:', error)
-      return res.status(500).end('Internal Server Error')
-    }
-
-    if (!data || data.length === 0) {
-      const updatedHtml = htmlData
-        .replace('__META_TITLE__', 'Artdle - Un dibujo al día')
-        .replace(
-          '__META_DESCRIPTION__',
-          '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
-        )
-        .replace('__META_OG_TITLE__', 'Artdle - Un dibujo al día')
-        .replace(
-          '__META_OG_DESCRIPTION__',
-          '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
-        )
-        .replace('__META_OG_IMAGE__', '/icon.png')
-        .replace('__META_TW_TITLE__', 'Artdle - Un dibujo al día')
-        .replace(
-          '__META_TW_DESCRIPTION__',
-          '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
-        )
-        .replace('__META_TW_IMAGE__', '/icon.png')
-
-      return res.status(200).send(updatedHtml)
-    }
-
-    const draw = data[0]
-    const updatedHtml = htmlData
-      .replace('__META_TITLE__', draw.name)
-      .replace('__META_DESCRIPTION__', draw.created_at)
-      .replace('__META_OG_TITLE__', draw.name)
-      .replace('__META_OG_DESCRIPTION__', draw.created_at)
-      .replace('__META_OG_IMAGE__', draw.uridata)
-      .replace('__META_TW_TITLE__', draw.name)
-      .replace('__META_TW_DESCRIPTION__', draw.created_at)
-      .replace('__META_TW_IMAGE__', draw.uridata)
-
-    return res.status(200).send(updatedHtml)
-  } catch (err) {
-    console.error('Error processing request:', err)
+  if (error) {
+    console.error('Error fetching data from Supabase:', error)
     return res.status(500).end('Internal Server Error')
   }
+
+  if (!data || data.length === 0) {
+    const updatedHtml = htmlData
+      .replace('__META_TITLE__', 'Artdle - Un dibujo al día')
+      .replace(
+        '__META_DESCRIPTION__',
+        '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
+      )
+      .replace('__META_OG_TITLE__', 'Artdle - Un dibujo al día')
+      .replace(
+        '__META_OG_DESCRIPTION__',
+        '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
+      )
+      .replace('__META_OG_IMAGE__', '/icon.png')
+      .replace('__META_TW_TITLE__', 'Artdle - Un dibujo al día')
+      .replace(
+        '__META_TW_DESCRIPTION__',
+        '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
+      )
+      .replace('__META_TW_IMAGE__', '/icon.png')
+
+    return res.status(200).send(updatedHtml)
+  }
+
+  const draw = data[0]
+  const updatedHtml = htmlData
+    .replace('__META_TITLE__', draw.name)
+    .replace('__META_DESCRIPTION__', draw.created_at)
+    .replace('__META_OG_TITLE__', draw.name)
+    .replace('__META_OG_DESCRIPTION__', draw.created_at)
+    .replace('__META_OG_IMAGE__', draw.uridata)
+    .replace('__META_TW_TITLE__', draw.name)
+    .replace('__META_TW_DESCRIPTION__', draw.created_at)
+    .replace('__META_TW_IMAGE__', draw.uridata)
+
+  return res.status(200).send(updatedHtml)
 }
