cbc1c3167da5490c1afd808df8a7def1cbae2875
arugerdev <alvaroch250@gmail.com>
2024-05-31 16:20:58 +0200

    Debug #3 middleware

--- middleware.js
+++ middleware.js
@@ -1,76 +1,78 @@
 import { createClient } from '@supabase/supabase-js'
-import path from 'path'
-import { fs } from 'fs'
+// import path from 'path'
+// import { fs } from 'fs'
 
 // dotenv.config()
 
 export default function handler (request, context) {
   const __filename = import.meta.url
-  const __dirname = path.dirname(__filename)
-  const indexPath = path.resolve(__dirname, 'dist', 'index.html')
-  fs.readFileSync(indexPath, 'utf8', (err, htmlData) => {
-    const drawId = request.url.split('/')[1]
 
-    context.waitUntil(
-      getDraw(drawId)
-        .then(data => {
-          if (!data) {
-            const updatedHtml = htmlData
-              .replace('__META_TITLE__', 'Artdle - Un dibujo al día')
-              .replace(
-                '__META_DESCRIPTION__',
-                '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
-              )
-              .replace('__META_OG_TITLE__', 'Artdle - Un dibujo al día')
-              .replace(
-                '__META_OG_DESCRIPTION__',
-                '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
-              )
-              .replace('__META_OG_IMAGE__', '/icon.png')
-              .replace('__META_TW_TITLE__', 'Artdle - Un dibujo al día')
-              .replace(
-                '__META_TW_DESCRIPTION__',
-                '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
-              )
-              .replace('__META_TW_IMAGE__', '/icon.png')
-            return new Response(updatedHtml)
-          }
-          const draw = data[0]
-          const updatedHtml = htmlData
-            .replace('__META_TITLE__', draw.name)
-            .replace('__META_DESCRIPTION__', draw.created_at)
-            .replace('__META_OG_TITLE__', draw.name)
-            .replace('__META_OG_DESCRIPTION__', draw.created_at)
-            .replace('__META_OG_IMAGE__', draw.uridata)
-            .replace('__META_TW_TITLE__', draw.name)
-            .replace('__META_TW_DESCRIPTION__', draw.created_at)
-            .replace('__META_TW_IMAGE__', draw.uridata)
+  console.log(request, context)
+  // const __dirname = path.dirname(__filename)
+  // const indexPath = path.resolve(__dirname, 'dist', 'index.html')
+  // fs.readFileSync(indexPath, 'utf8', (err, htmlData) => {
+  //   const drawId = request.url.split('/')[1]
 
-          return new Response(updatedHtml)
-        })
-        .catch(() => {
-          const updatedHtml = htmlData
-            .replace('__META_TITLE__', 'Artdle - Un dibujo al día')
-            .replace(
-              '__META_DESCRIPTION__',
-              '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
-            )
-            .replace('__META_OG_TITLE__', 'Artdle - Un dibujo al día')
-            .replace(
-              '__META_OG_DESCRIPTION__',
-              '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
-            )
-            .replace('__META_OG_IMAGE__', '/icon.png')
-            .replace('__META_TW_TITLE__', 'Artdle - Un dibujo al día')
-            .replace(
-              '__META_TW_DESCRIPTION__',
-              '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
-            )
-            .replace('__META_TW_IMAGE__', '/icon.png')
-          return new Response(updatedHtml)
-        })
-    )
-  })
+  //   context.waitUntil(
+  //     getDraw(drawId)
+  //       .then(data => {
+  //         if (!data) {
+  //           const updatedHtml = htmlData
+  //             .replace('__META_TITLE__', 'Artdle - Un dibujo al día')
+  //             .replace(
+  //               '__META_DESCRIPTION__',
+  //               '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
+  //             )
+  //             .replace('__META_OG_TITLE__', 'Artdle - Un dibujo al día')
+  //             .replace(
+  //               '__META_OG_DESCRIPTION__',
+  //               '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
+  //             )
+  //             .replace('__META_OG_IMAGE__', '/icon.png')
+  //             .replace('__META_TW_TITLE__', 'Artdle - Un dibujo al día')
+  //             .replace(
+  //               '__META_TW_DESCRIPTION__',
+  //               '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
+  //             )
+  //             .replace('__META_TW_IMAGE__', '/icon.png')
+  //           return new Response(updatedHtml)
+  //         }
+  //         const draw = data[0]
+  //         const updatedHtml = htmlData
+  //           .replace('__META_TITLE__', draw.name)
+  //           .replace('__META_DESCRIPTION__', draw.created_at)
+  //           .replace('__META_OG_TITLE__', draw.name)
+  //           .replace('__META_OG_DESCRIPTION__', draw.created_at)
+  //           .replace('__META_OG_IMAGE__', draw.uridata)
+  //           .replace('__META_TW_TITLE__', draw.name)
+  //           .replace('__META_TW_DESCRIPTION__', draw.created_at)
+  //           .replace('__META_TW_IMAGE__', draw.uridata)
+
+  //         return new Response(updatedHtml)
+  //       })
+  //       .catch(() => {
+  //         const updatedHtml = htmlData
+  //           .replace('__META_TITLE__', 'Artdle - Un dibujo al día')
+  //           .replace(
+  //             '__META_DESCRIPTION__',
+  //             '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
+  //           )
+  //           .replace('__META_OG_TITLE__', 'Artdle - Un dibujo al día')
+  //           .replace(
+  //             '__META_OG_DESCRIPTION__',
+  //             '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
+  //           )
+  //           .replace('__META_OG_IMAGE__', '/icon.png')
+  //           .replace('__META_TW_TITLE__', 'Artdle - Un dibujo al día')
+  //           .replace(
+  //             '__META_TW_DESCRIPTION__',
+  //             '¿Cuál será la palabra de hoy? Entra ahora en Artdle.com, descubrelo y dibuja!'
+  //           )
+  //           .replace('__META_TW_IMAGE__', '/icon.png')
+  //         return new Response(updatedHtml)
+  //       })
+  //   )
+  // })
 }
 
 export const config = {
