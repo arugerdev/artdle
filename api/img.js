55a9878cbf58ca5abe653d85a85276aa050c1e3c
arugerdev <alvaroch250@gmail.com>
2024-05-31 19:44:37 +0200

    Change decode uri data to image

--- api/img.js
+++ api/img.js
@@ -39,37 +39,6 @@ export const config = {
   }
 }
 
-WebPDecodeAndDraw = function (data) {
-  var decoder = new WebPDecoder()
-
-  var bitmap = decoder.WebPDecode(data, data.length)
-
-  if (bitmap) {
-    //Draw Image
-    var output = ctx.createImageData(canvas.width, canvas.height)
-    var biWidth = canvas.width
-    var outputData = output.data
-    for (var h = 0; h < canvas.height; h++) {
-      for (var w = 0; w < canvas.width; w++) {
-        outputData[0 + w * 4 + biWidth * 4 * h] =
-          bitmap[0 + w * 4 + biWidth * 4 * h]
-        outputData[1 + w * 4 + biWidth * 4 * h] =
-          bitmap[1 + w * 4 + biWidth * 4 * h]
-        outputData[2 + w * 4 + biWidth * 4 * h] =
-          bitmap[2 + w * 4 + biWidth * 4 * h]
-        outputData[3 + w * 4 + biWidth * 4 * h] =
-          bitmap[3 + w * 4 + biWidth * 4 * h]
-      }
-    }
-
-    ctx.putImageData(output, 0, 0)
-
-    var dataURL = canvas.toDataURL('image/png')
-
-    document.getElementById('dec').src = dataURL
-  }
-}
-
 function getImage (img) {
   // Create an empty canvas element
   canvas = document.createElement('canvas')
@@ -79,8 +48,5 @@ function getImage (img) {
   // Copy the image contents to the canvas
   ctx = canvas.getContext('2d')
   ctx.drawImage(img, 0, 0)
-
-  WebPDecodeAndDraw(
-    ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)['data']
-  )
+  return canvas.toDataURL('image/png')
 }
