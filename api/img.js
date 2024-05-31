61ad58b7813ecd16743dc3b9fc09196bcf1c9683
arugerdev <alvaroch250@gmail.com>
2024-06-01 00:55:39 +0200

    Change share image response

--- api/img.js
+++ api/img.js
@@ -27,18 +27,43 @@ export default async function handler (req, res) {
     return res.status(500).json({ error: 'Internal server error' })
   }
 
-  try {
-    const base64Data = data.uridata.replace(/^data:image\/png;base64,/, '')
+  // try {
+  //   const base64Data = data.uridata.replace(/^data:image\/png;base64,/, '')
 
-    const imageBuffer = Buffer.from(base64Data, 'base64')
+  //   const imageBuffer = Buffer.from(base64Data, 'base64')
 
-    res.setHeader('Content-Type', 'image/jpg')
-    res.send(imageBuffer)
-    return res.status(200)
-  } catch (fetchError) {
-    console.error(fetchError)
-    return res.status(500).json({ error: 'Failed to fetch image' })
-  }
+  //   res.setHeader('Content-Type', 'image/jpg')
+  //   res.send(imageBuffer)
+  //   return res.status(200)
+  // } catch (fetchError) {
+  //   console.error(fetchError)
+  //   return res.status(500).json({ error: 'Failed to fetch image' })
+  // }
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: 'black',
+          fontWeight: 900,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <img src={data.uridata} />
+        <p>{data.name}</p>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630
+    }
+  )
 }
 
 export const config = {
